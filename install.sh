606634d7a6055f58c086a90cf061a0815ac48d6d
WimRoose <systeembeheer@vooruit.be>
2015-02-25 17:07:13 +0100

    added libav-tools install

--- install.sh
+++ install.sh
@@ -5,4 +5,7 @@ if [ "$1" = "master" ]; then
 	sudo ln -s ~/vwall/gpio_master.conf /etc/supervisor/conf.d/gpio_master.conf
 	sudo /etc/init.d/supervisor stop > /dev/null
 	sudo /etc/init.d/supervisor start > /dev/null
+
+	echo "Installing avconv..."
+	sudo apt-get install libav-tools
 fi
