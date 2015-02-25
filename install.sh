457879f8dbcc7242bb585fe04b312c2a445cabc5
WimRoose <systeembeheer@vooruit.be>
2015-02-25 17:04:18 +0100

    added install.sh

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+if [ "$1" = "master" ]; then
+	echo "Adding gpio_master to autostart (via Supervisord)"
+	sudo ln -s ~/vwall/gpio_master.conf /etc/supervisor/conf.d/gpio_master.conf
+	sudo /etc/init.d/supervisor stop > /dev/null
+	sudo /etc/init.d/supervisor start > /dev/null
+fi
